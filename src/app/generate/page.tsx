7fb43f0942eca16b3711155c20983479f603d5fa
Joel <58995538+jtpotato@users.noreply.github.com>
2023-05-07 16:37:31 +1000

    remove ds store

--- src/app/generate/page.tsx
+++ src/app/generate/page.tsx
@@ -7,13 +7,14 @@ import ChooseModel from "./ChooseModel";
 function Generate() {
     const [token, setToken] = useState<string | null>(null);
     const [model, setModel] = useState<string | null>(null);
+    const [prompt, setPrompt] = useState<string | null>(null);
 
   return (
     <>
       <div className="bg-black h-screen w-screen text-white">
         <TokenInput updateToken={setToken} />
         <ChooseModel updateModel={setModel} />
-        <textarea placeholder="Prompt"></textarea>
+        <textarea placeholder="Prompt" onChange={(event) => setPrompt(event.target.value)}></textarea>
       </div>
     </>
   );
